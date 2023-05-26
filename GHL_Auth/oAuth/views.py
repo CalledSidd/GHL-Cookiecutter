9e20a60860aa16e2959545b039c838507480e88d
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-27 01:40:33 +0530

    added url for the access code

--- GHL_Auth/oAuth/views.py
+++ GHL_Auth/oAuth/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.conf import settings
+
 # Create your views here.
 class BaseView(View):
     template = 'auth.html'
     def get(self, request):
+       url = settings.GHL_URL
+       print(url)
        code =  request.GET.get('code')
        print(code)
        context = {
-           'code' : code
+           'code' : code,
+           'url' : url
        }
        return render(request, self.template, context)
