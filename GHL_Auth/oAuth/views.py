1f557a2256b58abd39786f789c4a5a881d1c30ec
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-27 00:38:13 +0530

    first base view working

--- GHL_Auth/oAuth/views.py
+++ GHL_Auth/oAuth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class BaseView(View):
+    template = 'auth.html'
+    def get(self, request):
+       code =  request.GET.get('code')
+       print(code)
+       context = {
+           code : 'code'
+       }
+       return render(request, self.template, context)
