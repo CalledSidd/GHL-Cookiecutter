0e7e42f28faa5937bd856cc6a19763d448c909b1
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-28 00:15:31 +0530

    added model and saving data

--- GHL_Auth/oAuth/models.py
+++ GHL_Auth/oAuth/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Api_Key_Data(models.Model):
     access_token = models.CharField(max_length=100)
     companyId = models.CharField(max_length=100)
-    access_expires_on = models.IntegerField()
+    access_expires_in = models.IntegerField()
     locationId = models.CharField(max_length=100, unique=True)
     refresh_token = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
--- GHL_Auth/oAuth/views.py
+++ GHL_Auth/oAuth/views.py
@@ -37,7 +37,15 @@ class BaseView(View):
         vals = access.json()
         pprint(vals)
         print(vals['access_token'])
-        
+
+        data = Api_Key_Data(
+            access_token = vals['access_token'],
+            companyId = vals['companyId'],
+            access_expires_in = vals['expires_in'],
+            locationId = vals['locationId'],
+            refresh_token = vals['refresh_token'],
+        )
+
 
 
     def get(self, request):
