f36959d12490033560f2a2ff0077029c9c5c94d2
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-27 19:37:49 +0530

    models done

--- GHL_Auth/oAuth/models.py
+++ GHL_Auth/oAuth/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Api_Key_Data(models.Model):
-    ghl_location_id = models.CharField(max_length=50, unique=True)
-    ghl_location_name = models.TextField()
-    ghl_oauth_token = models.CharField(max_length=50)
-    ghl_refresh_token = models.CharField(max_length=50)
-    ghl_oauth_token_expires_on = models.IntegerField()
-    ghl_location_api_key = models.TextField()
+    access_token = models.CharField(max_length=100)
+    companyId = models.CharField(max_length=100)
+    access_expires_on = models.IntegerField()
+    locationId = models.CharField(max_length=100, unique=True)
+    refresh_token = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
