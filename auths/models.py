4ea15087d6263b88d91e83b63b84dbb182451c2d
CalledSidd <sidharthsidhu12317@gmail.com>
2023-10-18 20:54:46 +0530

    new cookiecutter template made

--- auths/models.py
+++ auths/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Api_Key_Data(models.Model):
-    access_token = models.CharField(max_length=100)
+class Authtokendata(models.Model):
+    access_token = models.TextField()
     companyId = models.CharField(max_length=100)
     access_expires_in = models.IntegerField()
     locationId = models.CharField(max_length=100, unique=True)
